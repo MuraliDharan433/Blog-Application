51d97d384750e29355f8c9b97dd7719574e75ff8
Murali <121626892+MuraliDharan433@users.noreply.github.com>
2025-11-10 11:11:00 +0530

    Update API base URL to include '/api' path

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://blog-application-1-2jag.onrender.com",
+  baseURL: "https://blog-application-1-2jag.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
